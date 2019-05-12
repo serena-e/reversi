534c72a109d060c70761fca94cd4ec39295bee16
Serena <epstein.serena@gmail.com>
2019-05-12 13:34:46 -0700

    reversi app server setup

---
+++ server.js
@@ -0,0 +1,31 @@
+/* Include the static file webserver library */
+var static = require('node-static');
+
+/* Include the HTTP server library */
+var http = require('http');
+
+/* Assume that we are running on Heroku */
+var port = process.env.PORT;
+var directory = __dirname + 'public';
+
+/* If we aren't on Heroku, then we need to readjust the port and directory info and we know that because a port won't be set */
+if(typeof port == 'undefined' || !port){
+  directory = './public';
+  port = 8080;
+}
+
+/* Set up a static web server that will deliver files from the filesystem */
+var file = new static.Server(directory);
+
+/* Construct an HTTP server that gets files from the file server */
+var app = http.createServer(
+  function(request,response){
+    request.addListener('end',
+      function(){
+        file.serve(request,response);
+      }
+    ).resume();
+  }
+).listen(port);
+
+console.log('The server is running');
