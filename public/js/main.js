f5fe48eb76cff3ff9d6e025f663d40f381d61442
Serena <epstein.serena@gmail.com>
2019-05-19 18:48:54 -0700

    URL data & lobby usernames

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+/* Functions for general use */
+
+/* Returns the value associated with 'whichParam' on the URL */
+function getURLParameters(whichParam)
+{
+  var pageURL = window.location.search.substring(1);
+  var pageURLVariables = pageURL.split('&');
+  for(var i = 0; i < pageURLVariables.length; i++){
+    var parameterName = pageURLVariables[i].split('=');
+    if(parameterName[0] == whichParam){
+      return parameterName[1];
+    }
+  }
+}
+
+var username = getURLParameters('username');
+if('undefined' == typeof username || !username){
+  username = 'Anonymous_'+Math.random();
+}
+
+$('#messages').append('<h3>'+username+'</h3>');
